45a8fdfd12aaf68517f09d568636759741f1dba3
andigagaga <andigagaga1@gmail.com>
2023-07-23 22:21:03 +0700

    day-13

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"batch48/connection"
+	"context"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -24,34 +26,35 @@ type Project struct {
 }
 
 var dataProject = []Project{
-	{
-		Id:        0,
-		Name:      "Project 1",
-		StarDate:  "15-05-2023",
-		EndDate:   "15-06-2023",
-		Duration:  "1 bulan",
-		Detail:    "Bootcamp sebulan gaes",
-		Playstore: true,
-		Android: true,
-		Java: true,
-		React: true,
-	},
+	// {
+	// 	Id:        0,
+	// 	Name:      "Project 1",
+	// 	StarDate:  "15-05-2023",
+	// 	EndDate:   "15-06-2023",
+	// 	Duration:  "1 bulan",
+	// 	Detail:    "Bootcamp sebulan gaes",
+	// 	Playstore: true,
+	// 	Android:   true,
+	// 	Java:      true,
+	// 	React:     true,
+	// },
 
-	{
-		Id:        1,
-		Name:      "Project 2",
-		StarDate:  "15-05-2023",
-		EndDate:   "15-06-2023",
-		Duration:  "1 bulan",
-		Detail:    "Bootcamp sebulan gaes hehe",
-		Playstore: true,
-		Android: true,
-		Java: true,
-		React: true,
-	},
+	// {
+	// 	Id:        1,
+	// 	Name:      "Project 2",
+	// 	StarDate:  "15-05-2023",
+	// 	EndDate:   "15-06-2023",
+	// 	Duration:  "1 bulan",
+	// 	Detail:    "Bootcamp sebulan gaes hehe",
+	// 	Playstore: true,
+	// 	Android:   true,
+	// 	Java:      true,
+	// 	React:     true,
+	// },
 }
 
 func main() {
+	connection.DataBaseConnect()
 	e := echo.New()
 
 	// e = echo package nyaa...
@@ -63,7 +66,9 @@ func main() {
 
 	e.GET("/hello", helloWolrd)
 	e.GET("/index", home)
-	e.GET("/addproject", project)
+	e.GET("/addproject-data", project)
+	e.GET("/formaddproject", formproject)
+
 	e.GET("/testimonial", testimonial)
 	e.GET("/contact", contact)
 	e.GET("/project-detail/:id", projectDetail)
@@ -72,7 +77,7 @@ func main() {
 	// rout post nya
 	e.POST("/delete-addproject/:id", deleteProject)
 	e.POST("/edit-addproject/:id", submitEditedProject)
-	e.POST("/index", saveProject)
+	e.POST("/addproject", saveProject)
 
 	e.Logger.Fatal(e.Start(":1234"))
 }
@@ -82,17 +87,32 @@ func helloWolrd(c echo.Context) error {
 func home(c echo.Context) error {
 	var tmpl, err = template.ParseFiles("views/index.html")
 
+	data, _ := connection.Conn.Query(context.Background(), "SELECT id, name, start_date, end_date, duration, detail, playstore, android, java, react FROM tb_project")
+
+	var result []Project
+	for data.Next() {
+		var each = Project{}
+		err := data.Scan(&each.Id, &each.Name, &each.StarDate, &each.EndDate, &each.Duration, &each.Detail, &each.Playstore, &each.Android, &each.Java, &each.React)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+		result = append(result, each)
+
+	}
+
+
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 	}
 
-	Projects := map[string]interface{}{
-		"Projects" : dataProject,
-		
+	dataIndex := map[string]interface{}{
+		"Blogs": result,
 	}
+	fmt.Println("ini data index", dataIndex)
 
-
-	return tmpl.Execute(c.Response(), Projects)
+	return tmpl.Execute(c.Response(), dataIndex)
 }
 func project(c echo.Context) error {
 	var tmpl, err = template.ParseFiles("views/addproject.html")
@@ -100,6 +120,20 @@ func project(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 	}
+	Projects := map[string]interface{}{
+		"Projects": dataProject,
+	}
+
+	return tmpl.Execute(c.Response(), Projects)
+}
+
+func formproject(c echo.Context) error {
+	var tmpl, err = template.ParseFiles("views/formaddproject.html")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
 	return tmpl.Execute(c.Response(), nil)
 }
 
@@ -113,20 +147,20 @@ func projectDetail(c echo.Context) error {
 	for i, data := range dataProject {
 		if id == i {
 			ProjectDetail = Project{
-				Name:           data.Name,
-				StarDate:       data.StarDate,
-				EndDate:    	data.EndDate,
-				Duration:   	data.Duration,
-				Detail:         data.Detail,
-				Playstore:      data.Playstore,
-				Android:        data.Android,
-				Java:           data.Java,
-				React:          data.React,
+				Name:      data.Name,
+				StarDate:  data.StarDate,
+				EndDate:   data.EndDate,
+				Duration:  data.Duration,
+				Detail:    data.Detail,
+				Playstore: data.Playstore,
+				Android:   data.Android,
+				Java:      data.Java,
+				React:     data.React,
 			}
 		}
 	}
 	data := map[string]interface{}{
-		"Project":   ProjectDetail,
+		"Project": ProjectDetail,
 	}
 
 	var tmpl, err = template.ParseFiles("views/project-detail.html")
@@ -144,21 +178,21 @@ func editProject(c echo.Context) error {
 	for i, data := range dataProject {
 		if id == i {
 			ProjectDetail = Project{
-				Name:           data.Name,
-				StarDate:       data.StarDate,
-				EndDate:    	data.EndDate,
-				Duration:   	data.Duration,
-				Detail:         data.Detail,
-				Playstore:      data.Playstore,
-				Android:        data.Android,
-				Java:           data.Java,
-				React:          data.React,
+				Name:      data.Name,
+				StarDate:  data.StarDate,
+				EndDate:   data.EndDate,
+				Duration:  data.Duration,
+				Detail:    data.Detail,
+				Playstore: data.Playstore,
+				Android:   data.Android,
+				Java:      data.Java,
+				React:     data.React,
 			}
 		}
 	}
 	data := map[string]interface{}{
-		"Project":   ProjectDetail,
-		"Id": id,
+		"Project": ProjectDetail,
+		"Id":      id,
 	}
 
 	var tmpl, err = template.ParseFiles("views/editproject.html")
@@ -170,15 +204,15 @@ func editProject(c echo.Context) error {
 
 }
 func submitEditedProject(c echo.Context) error {
-	
+
 	// Menangkap Id dari Query Params
-	id, _:= strconv.Atoi(c.Param("id"))
+	id, _ := strconv.Atoi(c.Param("id"))
 
 	name := c.FormValue("input-project-title")
 	starDate := c.FormValue("input-startdate")
-		endDate := c.FormValue("input-enddate")
-		detail := c.FormValue("input-description")
-		// checkbox
+	endDate := c.FormValue("input-enddate")
+	detail := c.FormValue("input-description")
+	// checkbox
 	var nodejs bool
 	if c.FormValue("nodejs") == "checked" {
 		nodejs = true
@@ -199,20 +233,20 @@ func submitEditedProject(c echo.Context) error {
 		typescript = true
 	}
 
-		var editedProject = Project{
-			Name:       name,
-			StarDate:   starDate,
-			EndDate:    endDate,
-			Duration:   countDuration(starDate, endDate),
-			Detail:      detail,
-			Playstore: nodejs,
-			Android: reactjs,
-			Java: nextjs,
-			React: typescript,
-		}
+	var editedProject = Project{
+		Name:      name,
+		StarDate:  starDate,
+		EndDate:   endDate,
+		Duration:  countDuration(starDate, endDate),
+		Detail:    detail,
+		Playstore: nodejs,
+		Android:   reactjs,
+		Java:      nextjs,
+		React:     typescript,
+	}
 
-		dataProject[id] = editedProject
-		return c.Redirect(http.StatusMovedPermanently, "/index")
+	dataProject[id] = editedProject
+	return c.Redirect(http.StatusMovedPermanently, "/addproject-data")
 }
 func countDuration(d1 string, d2 string) string {
 	date1, _ := time.Parse("2006-01-02", d1)
@@ -235,7 +269,6 @@ func countDuration(d1 string, d2 string) string {
 	return strconv.Itoa(days) + " hari"
 }
 
-
 // func untuk rout post nyaaa
 func saveProject(c echo.Context) error {
 	name := c.FormValue("input-project-title")
@@ -244,6 +277,7 @@ func saveProject(c echo.Context) error {
 	// ambil date input
 	date1 := c.FormValue("input-startdate")
 	date2 := c.FormValue("input-enddate")
+
 	// parse date input dan formatting
 	uDate1, _ := time.Parse("2006-01-02", date1)
 	starDate := uDate1.Format("2 Jan 2006")
@@ -292,21 +326,21 @@ func saveProject(c echo.Context) error {
 		Duration:  diffUse,
 		Detail:    detail,
 		Playstore: nodejs,
-		Android: reactjs,
-		Java: nextjs,
-		React: typescript,
+		Android:   reactjs,
+		Java:      nextjs,
+		React:     typescript,
 	}
 
 	dataProject = append(dataProject, newProject)
 
 	fmt.Println(dataProject)
 
-	return c.Redirect(http.StatusMovedPermanently, "/index")
+	return c.Redirect(http.StatusMovedPermanently, "/addproject-data")
 }
 func deleteProject(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
 	dataProject = append(dataProject[:id], dataProject[id+1:]...)
-	return c.Redirect(http.StatusMovedPermanently, "/index")
+	return c.Redirect(http.StatusMovedPermanently, "/addproject-data")
 }
 func testimonial(c echo.Context) error {
 	var tmpl, err = template.ParseFiles("views/testimonial.html")
